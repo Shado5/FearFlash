de23de57ef159a2b473c49097e9301cebb9a00e7
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-03-03 15:10:07 +1100

    Ai

--- Assets/Scripts/EnemyScripts/EnemyAi.cs
+++ Assets/Scripts/EnemyScripts/EnemyAi.cs
@@ -7,11 +7,12 @@ public class EnemyAi : MonoBehaviour
 {
     public GameObject player;
     public NavMeshAgent agent;
+    public Animator anim;
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(player.transform.position);
-        
+        anim.SetBool("IsRunning", true);
     }
 }
