b17b1692449ede47bbf67c4af24ec3d82e93b932
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-03-03 15:04:44 +1100

    NavMesh

---
+++ Assets/Scripts/EnemyScripts/EnemyAi.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public GameObject player;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+        
+    }
+}
