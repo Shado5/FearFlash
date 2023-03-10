9b50cad7a19fc0ced120853f5d7aae71eae94668
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-03-10 13:57:30 +1100

    tutorial

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int health = 3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "zombie1")
+        {
+            --health;
+        }
+    }
+    private void Update()
+    {
+        if(health == 0)
+        {
+            //SceneManager.
+        }
+    }
+}
